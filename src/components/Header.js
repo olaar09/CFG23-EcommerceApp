9a3188c20ce96c9442ff15a057f668ceb994bc85
DCowey <131886411+DCowey@users.noreply.github.com>
2023-05-02 16:24:34 +0100

    draft basic header - had to move logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,15 @@
 import React from "react";
+import Logo from './Home/beaute_logo_lb.svg'
+import './Header.css';
+
+const Header = () => {
+
+    return(
+
+        <div className="Header">
+            <img src={Logo} className="App-logo" alt="logo" />  
+        </div>
+    )
+}
+
+export default Header
