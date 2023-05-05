32d54a4b0e28f44af4e6dfb4f0acf4a5c91bffea
shacreative <s.glo@live.com>
2023-05-05 20:33:42 +0100

    removed tutor comments

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -7,7 +7,7 @@ const Products = () => {
     <div>
       <div className="products">
       <h1>Products Page</h1>
-      <Link to="/cart"><button>Link to cart page</button></Link>
+      {<Link to="/cart"><button>Link to cart page</button></Link>}
     </div>
     </div>
   );
