8b03531859a2a97a21f4051f5a603adc68a566f9
Livia Floricel <livia.floricel@gmail.com>
2023-05-06 00:57:17 +0100

    Products page - added images to the components.

--- src/components/Products/ProductDetails.js
+++ src/components/Products/ProductDetails.js
@@ -1,23 +1,20 @@
 import React from "react";
-import './ProductDetails.css';
-import { Link } from 'react-router-dom';
+import "./ProductDetails.css";
+import { Link } from "react-router-dom";
 
 function ProductDetails(props) {
-  const productName = "Matte Lipstick";
-  const productDescription = "Lips Product description";
-  const lipsDescription = "Lips description";
-  const faceDescription = "Face description";
-  const productFace = "Face";
-  const productLips = "Lips";
-  const productPrice = 20;
   return (
     <div className="productcard">
-      <div>Image</div>
-      <div className = "item__description">
-      <h2>{ productName} </h2>
-      <p>{ productDescription }</p>
-      <div className="product__price">£{ productPrice }</div>
-      <Link to="/cart"><button>Add to Cart</button></Link>
+      <div> <img
+          src={props.image}
+        /></div>
+      <div className="item__description">
+        <h2>{props.title} </h2>
+        <p>{props.description}</p>
+        <div className="product__price">£{props.price}</div>
+        <Link to="/cart">
+          <button>Add to Cart</button>
+        </Link>
       </div>
     </div>
   );
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,44 +1,129 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import ProductDetails from './ProductDetails';
+import React from "react";
+import { Link } from "react-router-dom";
+import ProductDetails from "./ProductDetails";
+import beauteProducts from "./beaute_products shot.jpg";
+import beauteEyes from "./eyes1.jpg";
+import beauteEyeliner from "./eyes2.jpg";
+import beauteFoundation from "./foundation1.jpg";
+import beautePowder from "./foundation2.jpg";
+import beauteLipstick from "./lipstick1.jpg";
+import beauteGloss from "./lipstick2.jpg";
 
-import './Products.css';
+import "./Products.css";
 
 function Products() {
+  const products = [
+    {
+      id: "p1",
+      image: beauteLipstick,
+      title: "MATTE LIPSTICK",
+      description: "A creamy rich formula with high colour payoff, in a no-shine matte finish.",
+      price: 20,
+    },
+    {
+      id: "p2",
+      image: beauteGloss,
+      title: "GLOSS LIPSTICK",
+      description: "A creamy rich formula with high colour payoff, in a shine finish.",
+      price: 20,
+    },
+    {
+      id: "p3",
+      image: beauteEyes,
+      title: "EYE SHADOW",
+      description: "A shadow formulated to prolong the look of your eye makeup.",
+      price: 20,
+    },
+    {
+      id: "p4",
+      image: beauteEyeliner,
+      title: "EYELINER",
+      description: "A shadow formulated to prolong the look of your eye makeup.",
+      price: 20,
+    },
+    {
+      id: "p5",
+      image: beauteFoundation,
+      title: "MINERAL FOUNDATION",
+      description: "Light to medium beige with peachy undertone for light to medium skin.",
+      price: 20,
+    },
+    {
+      id: "p6",
+      image: beautePowder,
+      title: "POWDER FOUNDATION",
+      description: "Light to medium beige with peachy undertone for light to medium skin.",
+      price: 20,
+    },
+  ];
   return (
-    <div>
+    <>
       <div className="top">
-      <h1>PRODUCTS</h1>
-      <div>Image</div>
+        <h1 className="productsTitle">PRODUCTS</h1>
+        <img
+          src={beauteProducts}
+          className="beauteProducts"
+          alt="beauteProducts"
+        />
       </div>
       <div className="products">
-      <div className="categories">
-        <button>Filter</button>
-        <button>Lips</button>
-        <button>Eyes</button>
-        <button>Face</button>
+        <div className="categories">
+          <button>Filter</button>
+          <button>Lips</button>
+          <button>Eyes</button>
+          <button>Face</button>
+        </div>
+        <div className="products-list">
+          <h1>LIPS</h1>
+          <div className="lips">
+            <ProductDetails
+              image={products[0].image}
+              title={products[0].title}
+              description={products[0].description}
+              price={products[0].price}
+            ></ProductDetails>
+            <ProductDetails
+              image={products[1].image}
+              title={products[1].title}
+              description={products[1].description}
+              price={products[1].price}
+            ></ProductDetails>
+          </div>
+          <h1>EYES</h1>
+          <div className="eyes">
+            <ProductDetails
+              image={products[2].image}
+              title={products[2].title}
+              description={products[2].description}
+              price={products[2].price}
+            ></ProductDetails>
+            <ProductDetails
+              image={products[3].image}
+              title={products[3].title}
+              description={products[3].description}
+              price={products[3].price}
+            ></ProductDetails>
+          </div>
+          <h1>FACE</h1>
+          <div className="face">
+            <ProductDetails
+              image={products[4].image}
+              title={products[4].title}
+              description={products[4].description}
+              price={products[4].price}
+            ></ProductDetails>
+            <ProductDetails
+              image={products[5].image}
+              title={products[5].title}
+              description={products[5].description}
+              price={products[5].price}
+            ></ProductDetails>
+          </div>
+        </div>
       </div>
-      <div className="products-list">
-      <h1>LIPS</h1>
-      <div className="lips">
-      <ProductDetails></ProductDetails>
-      <ProductDetails></ProductDetails>
-      </div>
-      <h1>EYES</h1>
-      <div className="eyes">
-      <ProductDetails></ProductDetails>
-      <ProductDetails></ProductDetails>
-      </div>
-      <h1>FACE</h1>
-      <div className="face">
-      <ProductDetails></ProductDetails>
-      <ProductDetails></ProductDetails>
-      </div>
-      </div>
-    </div>
-  </div>
+    </>
   );
-} 
+}
 
 export default Products;
 
@@ -92,5 +177,3 @@ export default function product(props) {
     </div>
   )
 } */
-
-
