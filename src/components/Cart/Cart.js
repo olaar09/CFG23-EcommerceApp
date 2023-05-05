4a9862730d71977b3881dd91c7c0a3808642c0a0
Rosejean Uy <roseesuy@gmail.com>
2023-05-05 20:54:50 +0100

    cart page, search bar

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,26 @@
 import React from "react";
 import './Cart.css';
+import cartModel from './Model_for_cart.jpg';
 
 const Cart = () => {
+  const payNow = () => {
+  }
   return (
-    <div className="cart">
-    <div>
-      <h1>Cart Page</h1>
+    <>
+    <div id="content">
+      <img src={cartModel} className="cartModel" alt="cartModel" />
+      <div><h1 className="cartTitle">CART</h1></div>
     </div>
-  </div>
+    <div className="blockRight">
+      <h1 className="orderSummary">ORDER SUMMARY</h1>
+      <h3><span style={{ fontSize: 20 }}>Subtotal</span></h3>
+      <h3><span style={{ fontSize: 20 }}>Gift Wrap</span></h3>
+      <h3><span style={{ fontSize: 20 }}>Delivery: Standard Shipping</span></h3>
+      <h3 className="estTotal">Estimated Total</h3>
+      <button className="payButton" onClick={() => payNow()}>PAY NOW</button>
+    </div>
+    </>
   );
-}
+  }
 
 export default Cart;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Context from './context/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <Context> */}
+      <App />
+    {/* </Context> */}
   </React.StrictMode>
 );
 
