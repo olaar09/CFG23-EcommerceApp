aa2af01d05992e7f795b9395d4cae5ee917d4631
Rosejean Uy <roseesuy@gmail.com>
2023-05-15 11:41:14 +0100

    updated cart page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -15,31 +15,34 @@ const Cart = () => {
       <img src={cartModel} className="cartModel" alt="cartModel" />
       <div><h1 className="cartTitle">CART</h1></div>
     </div>
-    <div className="blockRight">
-      {productsCount > 0 ? 
+
+    {productsCount > 0 ? 
         <>
-          <p className="itemsLeft">Items in your cart:</p>
           <div className="itemsLeft">
+          <p>Items in your cart:</p>
           {cart.items.map((currentProduct, idx) => (
             // <h1>{currentProduct.id}</h1>
             <CartProduct 
               key={idx} 
               id={currentProduct.id}
-              quantity={currentProduct.quantity}></CartProduct>
+              quantity={currentProduct.quantity}>
+            </CartProduct>
             ))}</div>
+            <div className="blockRight">
             <h1 className="orderSummary">ORDER SUMMARY</h1>
             <h3><span style={{ fontSize: 20 }}>Subtotal</span></h3>
             <h3><span style={{ fontSize: 20 }}>Gift Wrap</span> <p className="priceRight">£0.00</p></h3>
             <h3><span style={{ fontSize: 20 }}>Delivery: Standard Shipping</span> <p className="priceRight">FREE</p></h3>
             <h3 className="estTotal">Estimated Total {cart.getTotalCost()}</h3>
             <button className="payButton" onClick={() => payNow()}>PAY NOW</button>
+            </div>
         </>
       :
-        <h2>YOUR CART IS EMPTY</h2>
+        <h2 className="blockRight">YOUR CART IS EMPTY</h2>
         }
-    </div>
     </>
   );
   }
+  
 
  export default Cart;
